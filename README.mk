572ad3b62466c26d6915c46b2a97c5cbb881b9b9
julien <juju.delmotte@gmail.com>
2024-01-12 09:54:24 +0100

    Ajout README

---
+++ README.mk
@@ -0,0 +1,24 @@
+# Mode d'emploi du Livrable :
+
+1) Telecharger tous les fichiers et décompresser le dossier
+2) Lancer les fichiers sur Matlab
+3) Taper "main()" dans la commande de Matlab
+4) Une fenetre va s'ouvrir !
+5) Choisisser la grandeur souhaitée
+6) Renseigner la Temperature et masse volumique voulu
+7) Cliquer sur Calculer
+
+
+# Norme utilisé dans le Code :
+
+## Normes Générales :
+- Toutes les fonctions théoriques sont disponnibles dans le dossier théorie.
+- Les arguments des fonctions sont dans l'ordre suivant : (delta,tau)
+- La dériviée d'une fonction se note : NomFonction_NomDeVariable
+
+## Normes Pour les fonctions Phir, DELTA, PSI, THETA et leurs dérivés : 
+
+- Si : Somme n°i de la fonction
+- Si,j : Sous-somme n°j de la somme i
+- Pi,j : sous-produit n°j de la somme j
+- ei : Exposant utile dans la somme i
